6f86d952cc7acee4a9bfff900894145b85d7a05b
bsquidwrd <git@bsquid.io>
2023-12-02 14:45:42 -0800

    Advent Day 2: Refactor and implement part 2

--- src/2023/day02/main.go
+++ src/2023/day02/main.go
@@ -10,7 +10,24 @@ import (
 )
 
 func main() {
-	part1()
+	var games []Game
+
+	filePath, err := filepath.Abs("./input.txt")
+	checkErr(err)
+
+	inputFile, err := os.Open(filePath)
+	checkErr(err)
+	defer inputFile.Close()
+	inputScanner := bufio.NewScanner(inputFile)
+
+	for inputScanner.Scan() {
+		lineContent := inputScanner.Text()
+		game := assembleGame(lineContent)
+		games = append(games, game)
+	}
+
+	part1(games)
+	part2(games)
 }
 
 func checkErr(err error) {
@@ -32,6 +49,9 @@ type Game struct {
 
 func isGameViable(game Game, totalRed int, totalGreen int, totalBlue int) bool {
 	for _, draw := range game.Draws {
+		if draw.red == 0 && draw.blue == 0 && draw.green == 0 {
+			return false
+		}
 		if draw.red > totalRed || draw.green > totalGreen || draw.blue > totalBlue {
 			return false
 		}
@@ -40,53 +60,61 @@ func isGameViable(game Game, totalRed int, totalGreen int, totalBlue int) bool {
 }
 
 func parseColorCount(s string, color string) int {
-	result := 0
-	if strings.Contains(s, color) {
-		foundNumber, err := strconv.Atoi(strings.TrimSpace(strings.Replace(s, color, "", 0)))
+	splitString := strings.Fields(s)
+	if strings.ToLower(splitString[1]) == strings.ToLower(color) {
+		foundNumber, err := strconv.Atoi(splitString[0])
 		if err != nil {
 			foundNumber = 0
 		}
-		result = foundNumber
+		return foundNumber
 	}
+	return 0
+}
+
+func getMinumumViableDrawing(game Game) Drawing {
+	var result Drawing
+
+	for _, drawing := range game.Draws {
+		if drawing.red > result.red {
+			result.red = drawing.red
+		}
+		if drawing.blue > result.blue {
+			result.blue = drawing.blue
+		}
+		if drawing.green > result.green {
+			result.green = drawing.green
+		}
+	}
+
 	return result
 }
 
-func part1() {
+func assembleGame(lineContent string) Game {
+	gameSplit := strings.Split(lineContent, ":")
+	trimmedGameNumber := strings.TrimSpace(strings.Split(gameSplit[0], " ")[1])
+	gameNumber, err := strconv.Atoi(trimmedGameNumber)
+	checkErr(err)
+
+	game := Game{
+		Number: gameNumber,
+	}
+
+	for _, rawDrawing := range strings.Split(gameSplit[1], ";") {
+		var draw Drawing
+		for _, rawColor := range strings.Split(rawDrawing, ",") {
+			draw.red += parseColorCount(rawColor, "red")
+			draw.blue += parseColorCount(rawColor, "blue")
+			draw.green += parseColorCount(rawColor, "green")
+		}
+		game.Draws = append(game.Draws, draw)
+	}
+	return game
+}
+
+func part1(games []Game) {
 	totalRed := 12
 	totalGreen := 13
 	totalBlue := 14
-	var games []Game
-
-	filePath, err := filepath.Abs("./test_input.txt")
-	checkErr(err)
-
-	inputFile, err := os.Open(filePath)
-	checkErr(err)
-	defer inputFile.Close()
-	inputScanner := bufio.NewScanner(inputFile)
-
-	for inputScanner.Scan() {
-		lineContent := inputScanner.Text()
-		gameSplit := strings.Split(lineContent, ":")
-		gameNumber, err := strconv.Atoi(strings.TrimSpace(strings.Replace(gameSplit[0], "Game", "", 0)))
-		checkErr(err)
-
-		game := Game{
-			Number: gameNumber,
-		}
-
-		for _, rawDrawing := range strings.Split(gameSplit[1], ";") {
-			var drawing Drawing
-			for _, rawColor := range strings.Split(rawDrawing, ",") {
-				drawing.red = parseColorCount(rawColor, "red")
-				drawing.blue = parseColorCount(rawColor, "blue")
-				drawing.green = parseColorCount(rawColor, "green")
-			}
-		}
-
-		games = append(games, game)
-
-	}
 
 	sumViableGames := 0
 	for _, game := range games {
@@ -96,5 +124,16 @@ func part1() {
 		}
 	}
 
-	fmt.Println("Viable game sum:", sumViableGames)
+	fmt.Println("Part 1 answer:", sumViableGames)
+}
+
+func part2(games []Game) {
+	totalDrawingPower := 0
+	for _, game := range games {
+		minumViableDrawing := getMinumumViableDrawing(game)
+		drawingPower := minumViableDrawing.red * minumViableDrawing.green * minumViableDrawing.blue
+		totalDrawingPower += drawingPower
+	}
+
+	fmt.Println("Part 2 answer:", totalDrawingPower)
 }
